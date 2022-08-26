de5ed7af526ee57dd5492b35bdac4db477e07b37
zhengchao <1036717543@qq.com>
2022-08-26 17:55:58 +0800

    nest 组件完善

--- src/module/Framework.ElasticSearch.Nest.TestConsole/Program.cs
+++ src/module/Framework.ElasticSearch.Nest.TestConsole/Program.cs
@@ -14,28 +14,78 @@ namespace Framework.ElasticSearch.Nest.TestConsole
                 .LoadModules();
         }
 
-        static void Main(string[] args)
+
+        static async Task Main(string[] args)
         {
-            var manager = ApplicationConfiguration.Current.Provider.GetRequiredService<IndexingManager<Logs>>();
-            for (var i= 0;i< 1000;i++)
-            {
-                var stop = Stopwatch.StartNew();
-                stop.Start();
-                var result= manager.AddAsync(new Logs() { Content = Guid.NewGuid().ToString(), Level = Guid.NewGuid().ToString() }, "logs").ConfigureAwait(false).GetAwaiter().GetResult();
-                stop.Stop();
-                if (!result)
-                    Console.WriteLine("数据添加失败,索引asdasdasddddddddddddddddddddddddddddddddddddddddddddddddddddddddd" + i);
-                else
-                    Console.WriteLine($"数据添加成功,索引:{i},耗时:{stop.ElapsedMilliseconds}");
-            }
+            await BulkAllAsync();
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 异步单条添加
+        /// </summary>
+        /// <returns></returns>
+        static async Task SingleAddAsync()
+        {
+
+            var _manager = ApplicationConfiguration.Current.Provider.GetRequiredService<IndexingManager<Logs>>();
+            var stop = Stopwatch.StartNew();
+            stop.Start();
+            for (var i = 0; i < 100; i++)
+            {
+
+                await _manager.AddAsync(Logs.Get(i), "logs");
+                Console.WriteLine($"当前索引:{i}");
+
+            }
+            stop.Stop();
+            Console.WriteLine($"异步数据添加成功,耗时:{stop.ElapsedMilliseconds}");
+        }
+
+        /// <summary>
+        /// 异步批量添加
+        /// </summary>
+        /// <returns></returns>
+        static async Task BulkAsync()
+        {
+            var _manager = ApplicationConfiguration.Current.Provider.GetRequiredService<IndexingManager<Logs>>();
+            var stop = Stopwatch.StartNew();
+            stop.Start();
+            var bulkData = new List<Logs>();
+            for (var i = 0; i < 10000; i++)
+            {
+                bulkData.Add(Logs.Get(i));
+            }
+            await _manager.BulkAsync(bulkData, "logs");
+            stop.Stop();
+         
+            Console.WriteLine($"异步数据添加成功,耗时:{stop.ElapsedMilliseconds}");
+        }
+
+        static async Task BulkAllAsync()
+        {
+            var _manager = ApplicationConfiguration.Current.Provider.GetRequiredService<IndexingManager<Logs>>();
+          
+            var bulkData = new List<Logs>();
+            for (var i = 0; i < 10000; i++)
+            {
+                bulkData.Add(Logs.Get(i));
+            }
+            var stop = Stopwatch.StartNew();
+            stop.Start();
+            await _manager.BulkAllAsync(bulkData, "logs");
+            stop.Stop();
+            Console.WriteLine($"异步数据添加成功,耗时:{stop.ElapsedMilliseconds}");
         }
     }
 
     public class Logs
     { 
-        public string Content { get; set; }
+        public float Price { get; set; }
 
-        public string Level { get; set; }
+        public static Logs Get(float price)
+        {
+            return new Logs() { Price = price };
+        }
     }
 }
--- src/module/Framework.ElasticSearch.Nest/IndexingManager.cs
+++ src/module/Framework.ElasticSearch.Nest/IndexingManager.cs
@@ -1,4 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using Nest;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Framework.ElasticSearch.Nest
 {
@@ -19,6 +26,70 @@ namespace Framework.ElasticSearch.Nest
             var client = await _clientProvider.GetClientAsync();
             var response = await client.IndexAsync(entity, i => i.Index(indexName??nameof(T)));
             return response.IsValid;
+        }
+
+        /// <summary>
+        /// 批量插入 复杂的批量插入建议使用BulkAll配合BulkAllObservable
+        /// </summary>
+        /// <param name="entities"></param>
+        /// <param name="indexName"></param>
+        /// <returns></returns>
+        public async Task<bool> BulkAsync(IEnumerable<T> entities, string? indexName = null)
+        {
+            var client = await _clientProvider.GetClientAsync();
+            var asyncBulkIndexResponse = await client.BulkAsync(b => b
+                .Index(indexName ?? nameof(T))
+                .IndexMany(entities)
+            );
+            return asyncBulkIndexResponse.IsValid;
+        }
+
+        public async Task BulkAllAsync(IEnumerable<T> entities, string? indexName = null, Func<BulkResponseItemBase, T, bool>? retryPredicate = null, Action<BulkResponseItemBase, T>? documentLost = null)
+        {
+            var seenPages = 0;
+            var observableBulk =await BulkAllAsync(indexName??nameof(T), entities, r => r
+                .ContinueAfterDroppedDocuments()
+                .DroppedDocumentCallback((bulkResponseItem, item) =>
+                {
+                    Console.WriteLine($"{bulkResponseItem},{item}");
+                })
+            );
+            observableBulk.Wait(TimeSpan.FromMinutes(5), b => Interlocked.Increment(ref seenPages));
+            var waitHandle = new ManualResetEvent(false);
+            ExceptionDispatchInfo? exceptionDispatchInfo = null;
+
+            var observer = new BulkAllObserver(
+                onNext: response =>
+                {
+
+                },
+                onError: exception =>
+                {
+                    exceptionDispatchInfo = ExceptionDispatchInfo.Capture(exception);
+                    waitHandle.Set();
+                },
+                onCompleted: () => waitHandle.Set());
+
+            observableBulk.Subscribe(observer);
+
+            waitHandle.WaitOne();
+
+            exceptionDispatchInfo?.Throw();
+        }
+
+        private async Task<BulkAllObservable<T>> BulkAllAsync(string index,IEnumerable<T> documents,Func<BulkAllDescriptor<T>, BulkAllDescriptor<T>>? selector = null)
+        {
+            var client = await _clientProvider.GetClientAsync();
+            selector = selector ?? (s => s);
+            var observableBulk = client.BulkAll(documents, f => selector(f
+                .MaxDegreeOfParallelism(Environment.ProcessorCount)
+                .BackOffTime(TimeSpan.FromSeconds(10))
+                .BackOffRetries(2)
+                .Size(1000)
+                .RefreshOnCompleted()
+                .Index(index)
+            ));
+            return observableBulk;
         }
     }
 }
